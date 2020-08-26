fae494ba02fffcb44c7d55a63b7137d123979bf0
Abdelhak Ihadjadene <xenomshox69@gmail.com>
2020-08-26 16:52:41 +0100

    hasPermission middleware v3

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -7,14 +7,25 @@ exports.isLoggedIn = function (req, res, next) {
     res.status(400).redirect("/login");
 };
 
-exports.hasPermission = (permission) => {
+// ["admin", "create", "delete"]
+exports.hasPermission = (permissions) => {
     return async (req, res, next) => {
         try {
             let user = await User.findById(req.user.id).populate("roles");
-            if (user.roles.some((role) => role[permission])) return next();
-            console.log("You are not Authorized");
-            req.flash("error", "Not Authorized");
-            res.redirect("/");
+            if (
+                user.roles.some((role) => role["owner"]) ||
+                user.roles.some((role) => role["admin_privillage"])
+            )
+                return next();
+
+            permissions.forEach((permission) => {
+                if (!user.roles.some((role) => role[permission])) {
+                    console.log("You are not Authorized");
+                    req.flash("error", "Not Authorized");
+                    res.redirect("/");
+                }
+            });
+            return next();
         } catch (err) {
             return next(err);
         }
--- routes/Admin.js
+++ routes/Admin.js
@@ -1,30 +1,33 @@
-const router = require('express').Router(),
-    { isLoggedIn ,hasPermission} = require("../middlewares/middleware"),
-    WebSite=require("../Classes/WebSite");
+const router = require("express").Router(),
+    { isLoggedIn, hasPermission } = require("../middlewares/middleware"),
+    WebSite = require("../Classes/WebSite");
 
-router.all('*',isLoggedIn,hasPermission("admin_privillage"));
-router.route("/")
-    .get((req, res )=> {
-        res.render("Admin/index",{url:"/Admin/dashboard"});
-    });
-router.route('/:Admin')
-    .all((req,res,next)=>{
-        req.URL=decodeURI(require('url').parse(req.url).pathname);
+router.all("*", isLoggedIn, hasPermission(["admin_privillage"]));
+router.route("/").get((req, res) => {
+    res.render("Admin/index", { url: "/Admin/dashboard" });
+});
+router
+    .route("/:Admin")
+    .all((req, res, next) => {
+        req.URL = decodeURI(require("url").parse(req.url).pathname);
         next();
     })
-    .get((req,res)=>{
-        if(req.query.f!==undefined)
-            res.render("Admin/"+req.params.Admin,(err,html)=>{
-                if(err) res.status(404).render("Admin/error",{pageName:req.params.Admin,path:req.URL});
+    .get((req, res) => {
+        if (req.query.f !== undefined)
+            res.render("Admin/" + req.params.Admin, (err, html) => {
+                if (err)
+                    res.status(404).render("Admin/error", {
+                        pageName: req.params.Admin,
+                        path: req.URL,
+                    });
                 else res.send(html);
             });
-        else res.render("Admin/index",{url:"/Admin"+req.URL});
-
+        else res.render("Admin/index", { url: "/Admin" + req.URL });
     })
-    .post((req,res)=>{
-        WebSite.SaveDetails(req.body,(status,message)=>{
+    .post((req, res) => {
+        WebSite.SaveDetails(req.body, (status, message) => {
             res.status(status).send(message);
-        })
-    })
+        });
+    });
 
 module.exports = router;
